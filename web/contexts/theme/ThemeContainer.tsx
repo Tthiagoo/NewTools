2050c9aa618c71706480bfe2174fd8ea57ecfae5
Tthiagoo <51219408+Tthiagoo@users.noreply.github.com>
2020-09-28 01:01:42 -0300

    Iniciação e config do chakra

---
+++ web/contexts/theme/ThemeContainer.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import {
+	ColorModeProvider,
+  CSSReset,
+	ThemeProvider,
+} from "@chakra-ui/core";
+
+import {ThemeProvider as EmotionTheme} from 'emotion-theming'
+
+import theme from '../../styles/theme' 
+const ThemeContainer: React.FC = ({ children }) => {
+	return (
+		<ThemeProvider theme={theme}>
+			<ColorModeProvider value="dark">
+       <EmotionTheme theme={theme}>
+         <CSSReset/>
+         {children}
+       </EmotionTheme>
+      </ColorModeProvider>
+		</ThemeProvider>
+	);
+};
+export default ThemeContainer
